96a6188bcd00f5420661136449fbb5d0acb6972e
k2 <izmail.k2@gmail.com>
2022-11-17 19:05:31 +0300

    hw9 refactor stringTime and stringDate timeout 1000

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -15,7 +15,7 @@ function Clock() {
         stop();
         const timerId: number = window.setInterval(() => {
             setDate(new Date());
-        });
+        },1000);
         setTimerId(timerId);
     }
 
@@ -32,8 +32,8 @@ function Clock() {
         setShow(!show)
     }
 
-    const stringTime = date.toLocaleTimeString().slice(0, -2) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleDateString('ru') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringTime = date.toLocaleTimeString('ru-Ru')|| <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate = date.toLocaleDateString('ru-Ru') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
 // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = date.toLocaleDateString('en', {weekday: "long"}) || <br/> // пишут студенты
@@ -57,8 +57,8 @@ function Clock() {
                 <div className={s.more}>
                     {show ? (
                         <>
-                            <span id={'hw9-month'}>{stringMonth}</span>{' '}
-                            <span id={'hw9-date'}>{stringDate},</span>
+                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
+                            <span id={'hw9-date'}>{stringDate}</span>
                         </>
                     ) : (
                         <>
